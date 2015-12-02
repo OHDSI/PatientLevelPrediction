583dae283632bf3eaa065527c10b247ac62d9394
Marc Suchard <msuchard@gmail.com>
2015-12-02 15:11:27 -0500

    added drat deployment script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit -o nounset
+addToDrat(){
+  PKG_REPO=$PWD
+
+  cd ..; mkdir drat; cd drat
+
+  ## Set up Repo parameters
+  git init
+  git config user.name "Marc A. Suchard"
+  git config user.email "msuchard@ucla.edu"
+  git config --global push.default simple
+
+  ## Get drat repo
+  git remote add upstream "https://$GH_TOKEN@github.com/OHDSI/drat.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
+
+  Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
+    repodir = '.', \
+    commit='Travis update: build $TRAVIS_BUILD_NUMBER')"
+  git push
+
+}
+addToDrat
