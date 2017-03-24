17a1d2aad3051a81fac68408e439bc3d538fc8e1
Melissa Rost <melissa.rost@gtri.gatech.edu>
2017-03-23 21:51:50 -0400

    removed print statements

--- inst/python/create_files_doctor_ai.py
+++ inst/python/create_files_doctor_ai.py
@@ -2,8 +2,8 @@ import sys
 import os
 import cPickle
 
-# for each rowId find on the same day covarteIds and group them
-# create a list of [covairateIDs]
+# for each rowId find on the same day covariateIds and group them
+# create a list of [covariateIDs]
 filename = sys.argv[1]
 
 def get_key(key):
@@ -16,10 +16,7 @@ patient_dict = {}
 with open(filename) as fp:
     for lines in fp:
         lines = lines.strip('\n').strip('\r').split(',')
-#        print(lines, type(lines), lines[0])
-
         p_id, cov_id, time_id =  lines[1], lines[2], lines[3]
-        # print(p_id, cov_id, time_id)
         if p_id not in patient_dict:
             patient_dict[p_id] = {time_id: [cov_id]}
         else:
@@ -27,33 +24,23 @@ with open(filename) as fp:
                 patient_dict[p_id][time_id] = [cov_id]
             else:
                 patient_dict[p_id][time_id].append(cov_id)
-# print(patient_dict)
 print('{} Patients'.format(len(patient_dict)))
 
 covs_list = []
 time_list = []
 
 for patient, time_cov_dict in patient_dict.items():
-    # print('-------------------')
-    # print(patient, time_cov_dict)
     patient_cov_list = []
     patient_time_diffs  = []
     sorted_times = sorted(time_cov_dict, key=get_key)
     prev_time = sorted_times[0]
     for time in sorted_times:
-        diff = int(time) - int(prev_time)
+        patient_time_diffs.append(int(time) - int(prev_time))
+        patient_cov_list.append(time_cov_dict[time]) # all the covariates for that time
         prev_time = time
-        # print(diff)
-        patient_time_diffs.append(diff)
-        patient_cov_list.append(time_cov_dict[time])
-
-    # print(patient_cov_list)
     time_list.append(patient_time_diffs)
     covs_list.append(patient_cov_list)
-# print(time_list)
 
-print('DONE')
-# print(covs_list)
-# print(time_list)
 cPickle.dump(covs_list, open('visit.p', 'wb'))
 cPickle.dump(time_list,  open('time.p',  'wb'))
+print('DONE')
