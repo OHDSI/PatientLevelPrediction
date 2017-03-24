1e4396da4724c9a1cc143da571224913c0ea96f0
Melissa Rost <melissa.rost@gtri.gatech.edu>
2017-03-23 21:43:40 -0400

    improved variable names and fixed first record not being added to the patient list

--- inst/python/create_files_doctor_ai.py
+++ inst/python/create_files_doctor_ai.py
@@ -3,7 +3,7 @@ import os
 import cPickle
 
 # for each rowId find on the same day covarteIds and group them
-# create a list of [covairateIDs] 
+# create a list of [covairateIDs]
 filename = sys.argv[1]
 
 def get_key(key):
@@ -17,47 +17,43 @@ with open(filename) as fp:
     for lines in fp:
         lines = lines.strip('\n').strip('\r').split(',')
 #        print(lines, type(lines), lines[0])
-          
+
         p_id, cov_id, time_id =  lines[1], lines[2], lines[3]
-        print(p_id, cov_id, time_id)
+        # print(p_id, cov_id, time_id)
         if p_id not in patient_dict:
-            patient_dict[p_id] = {}
+            patient_dict[p_id] = {time_id: [cov_id]}
         else:
             if time_id not in patient_dict[p_id]:
-                patient_dict[p_id][time_id] = []
+                patient_dict[p_id][time_id] = [cov_id]
             else:
                 patient_dict[p_id][time_id].append(cov_id)
-print(patient_dict)
+# print(patient_dict)
+print('{} Patients'.format(len(patient_dict)))
 
-final_list = []
+covs_list = []
 time_list = []
-count = 0
 
-for kk in patient_dict.keys():
-    count +=1
-    print('-------------------')
-    print(kk, patient_dict[kk])
-    temp_list = []
-    time_list_temp  = []
-    vv = 1000
-    init = 0
-    for vv in sorted(patient_dict[kk], key=get_key, reverse=True):
-        if init==0:
-            diff = 0
-            prev_vv = vv
-        else:
-            diff = -1*(int(vv)-int(prev_vv))
-        init=1
-        print(diff)
-        time_list_temp.append(diff)
-        temp_list.append(patient_dict[kk][vv])
-    
-    print(temp_list)
-    time_list.append(time_list_temp)
-    final_list.append(temp_list)
-#print(final_list, len(final_list),count)
-print(time_list)
+for patient, time_cov_dict in patient_dict.items():
+    # print('-------------------')
+    # print(patient, time_cov_dict)
+    patient_cov_list = []
+    patient_time_diffs  = []
+    sorted_times = sorted(time_cov_dict, key=get_key)
+    prev_time = sorted_times[0]
+    for time in sorted_times:
+        diff = int(time) - int(prev_time)
+        prev_time = time
+        # print(diff)
+        patient_time_diffs.append(diff)
+        patient_cov_list.append(time_cov_dict[time])
 
-cPickle.dump(final_list, open('visit.p', 'wb')) 
+    # print(patient_cov_list)
+    time_list.append(patient_time_diffs)
+    covs_list.append(patient_cov_list)
+# print(time_list)
+
+print('DONE')
+# print(covs_list)
+# print(time_list)
+cPickle.dump(covs_list, open('visit.p', 'wb'))
 cPickle.dump(time_list,  open('time.p',  'wb'))
-
