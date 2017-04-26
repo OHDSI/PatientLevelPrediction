4ae7064a8ae21183a09476b0777563f3bb754232
xypan1232 <xypan172436@gmail.com>
2017-04-26 14:40:52 +0200

    fix small bugs

--- inst/python/deepLearning.py
+++ inst/python/deepLearning.py
@@ -19,16 +19,16 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_size)
                 
-                #self.fc2 = = nn.BatchNorm1d(hidden_size)
+        #self.fc2 = = nn.BatchNorm1d(hidden_size)
         self.fc2 = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-                x = F.dropout(x, training=self.training)
-                #x = F.relu(self.fc2(x))
-                #x = F.dropout(x, p=0.2, training=self.training)
+        x = F.dropout(x, training=self.training)
+        #x = F.relu(self.fc2(x))
+        #x = F.dropout(x, p=0.2, training=self.training)
 
-                x = self.fc2(x)
+        x = self.fc2(x)
                 #x = F.dropout(x, training=self.training)
         #x = F.tanh(self.fc2(x))
         x = F.sigmoid(x)
@@ -45,23 +45,27 @@ class MLP(nn.Module):
         return temp
             
 class CNN(nn.Module):
-    def __init__(self, nb_filter, kernel_size = 5, pool_size = 3, padding=2, labcounts = 32, window_size = 12, hidden_szie = 100):
+    def __init__(self, nb_filter, kernel_size = (1, 5), pool_size = (1, 3), labcounts = 32, window_size = 12, hidden_size = 100, stride = 1, padding = 0):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv1d(1, nb_filter, kernel_size, padding),
+            nn.Conv2d(1, nb_filter, kernel_size, padding),
             nn.BatchNorm1d(nb_filter),
             nn.ReLU(),
-            nn.MaxPool1d(pool_size))
+            nn.MaxPool2d(pool_size))
+        out1_size = (window_size + 2*padding - (kernel_size[1] - 1) - 1)/stride + 1
+        maxpool_size = (out1_size + 2*padding - (pool_size[1] - 1) - 1)/stride + 1
         self.layer2 = nn.Sequential(
-            nn.Conv1d(nb_filter, 2*nb_filter, kernel_size, padding),
+            nn.Conv2d(nb_filter, 2*nb_filter, kernel_size, padding),
             nn.BatchNorm1d(2*nb_filter),
             nn.ReLU(),
-            nn.MaxPool1d(pool_size))
+            nn.MaxPool2d(pool_size))
+        out2_size = (maxpool_size + 2*padding - (kernel_size[1] - 1) - 1)/stride + 1
+        maxpool2_size = (out2_size + 2*padding - (pool_size[1] - 1) - 1)/stride + 1
         self.drop1 = nn.Dropout(p=0.5)
-        self.fc1 = nn.Linear(window_size*labcounts*32, hidden_szie)
+        self.fc1 = nn.Linear(maxpool2_size*labcounts*2*nb_filter, hidden_size)
         self.drop2 = nn.Dropout(p=0.5)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_szie, 1)
+        self.fc2 = nn.Linear(hidden_size, 2)
         
     def forward(self, x):
         if type(x) is np.ndarray:
@@ -120,10 +124,11 @@ class RNN(nn.Module):
         x = Variable(x)
         if cuda:
             x = x.cuda()
-
-        # Set initial states 
-        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) 
-        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+            h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda() 
+            c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda()
+        else:
+            h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) 
+            c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
         
         # Forward propagate RNN
         out, _ = self.lstm(x, (h0, c0))  
@@ -148,11 +153,11 @@ class BiRNN(nn.Module):
         x = Variable(x)
         if cuda:
             x = x.cuda()
-
-        # Set initial states
-        h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)) # 2 for bidirection 
-        c0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size))
-        
+            h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)).cuda() # 2 for bidirection 
+            c0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)).cuda()
+        else:
+            h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)) # 2 for bidirection 
+            c0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size))
         # Forward propagate RNN
         out, _ = self.lstm(x, (h0, c0))
         
