6737223220ae18b885e12690f1a8475a91c49397
xypan1232 <xypan172436@gmail.com>
2017-10-10 14:28:26 +0200

    update doc for ensembling model

--- R/Ensemble.R
+++ R/Ensemble.R
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' ensemble - Create a cost curve for weighting classes using different weights
+#' ensemble - Create an ensembling model using different models
 #'
 #' @description
 #' #' 
