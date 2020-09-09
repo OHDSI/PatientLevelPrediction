f75b65cadac5655a7ff801ba2dadd95726024b6d
RossDWilliams <r.williams@erasmusmc.nl>
2020-09-09 13:39:10 +0200

    added initial recalibration, added exclude param to llr, needs fixing

--- R/Recalibration.R
+++ R/Recalibration.R
@@ -16,29 +16,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-recalibratePlp <- function(plpModel, validationData, testFraction = 25, population, method = c('slopeintercept', 'reestimate', 'intheLarge')){
+recalibratePlp <- function(plpResult, validationData, testFraction = 25, population, method = c('slopeintercept', 'reestimate', 'RecalibrationintheLarge')){
   # check input:
   if (is.null(population))
     stop("NULL population")
   if (class(validationData) != "plpData")
     stop("Incorrect plpData class")
-  if (class(plpModel) != "plpModel")
-    stop("Incorrect plpModel class")
+  if (class(plpResult) != "runPlp")
+    stop("plpResult is not of class runPlp")
   if(!method  %in% c('intercept', 'reestimate'))
     stop("Unknown recalibration method type")
+  if(method == 'RecalibrationintheLarge'){
+    observedFreq <- sum(population$outcomeCount>0) / nrow(population)
+    predictedFreq <- mean(plpResult$prediction$value)
+    misCal <- observedFreq - predictedFreq
+    recalResult <- plpResult
+    recalResult$prediction$value
+    recalResult$model$model$coefficients["(Intercept)"] <- plpResult$model$model$coefficients["(Intercept)"] + misCal 
+
+        #recalculate predictions
+    predictionOld <- plpResult$model$predict(plpData = plpData2, population = population2)
+    prediction <- recalResult$model$predict(plpData=plpData2,population=population2)
+    return(recalResult)
+  }
   if(method == 'slopeintercept'){
     # adjust slope intercept to 1,0
   }
-  if(method == 'recalibrationintheLarge'){
-    #match avg pred to obs risk, adjust intercept
-  }
+
   if(method == 'reestimate'){
-    covs <- plpModel$model$coefficients[plpModel$model$coefficients != 0]
+    covs <- plpResult$model$model$coefficients[plpResult$model$model$coefficients != 0]
     includeCovariateIds <- names(covs[-1])
-    setLassoRefit <- setLassoLogisticRegression()
+    noShrinkage <- append(0,includeCovariateIds)
+    setLassoRefit <- setLassoLogisticRegression(includeCovariateIds = includeCovariateIds,
+                                                noShrinkage = noShrinkage)
+    validationData$covariateData$covariates
+    containedIds <- RSQLite::dbGetQuery(validationData$covariateData, "SELECT DISTINCT covariateId FROM covariateRef")
+    setdiff(noShrinkage, containedIds$covariateId)
     result <- runPlp(population = population, 
                      plpData = validationData, 
-                     includeCovariateIds = includeCovariateIds,
                      modelSettings = setLassoRefit, 
                      testFraction = testFraction )
   }
