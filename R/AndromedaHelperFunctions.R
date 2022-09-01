265359bf0cf3ed99ccb13758a9d61b7041157baa
Egill Fridgeirsson <egillax@gmail.com>
2022-09-01 11:58:18 +0200

    fix Cyclops call

--- R/AndromedaHelperFunctions.R
+++ R/AndromedaHelperFunctions.R
@@ -41,7 +41,7 @@ limitCovariatesToPopulation <- function(covariateData, rowIds) {
 # temporary add cyclops function here so I don't need to fork cyclops
 #' @describeIn convertToCyclopsData Convert data from two \code{Arrow} FileSystemDatasets
 #' @export
-convertToCyclopsData.FileSystemDataset <- convertToCyclopsData.tbl_dbi
+convertToCyclopsData.FileSystemDataset <- Cyclops:::convertToCyclopsData.tbl_dbi
 
 # is this used?
 # return prev of ffdf 
