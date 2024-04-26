cc2c54eb42c509957c21992888347ef3afe08a2d
egillax <egillax@gmail.com>
2024-04-26 09:36:53 +0200

    remove version from is_installed

--- R/HelperFunctions.R
+++ R/HelperFunctions.R
@@ -16,10 +16,10 @@ removeInvalidString <- function(string){
 
 
 # Borrowed from devtools: https://github.com/hadley/devtools/blob/ba7a5a4abd8258c52cb156e7b26bb4bf47a79f0b/R/utils.r#L44
-is_installed <- function (pkg, version = 0) {
+is_installed <- function (pkg) {
   installed_version <- tryCatch(utils::packageVersion(pkg), 
                                 error = function(e) NA)
-  !is.na(installed_version) && installed_version >= version
+  !is.na(installed_version)
 }
 
 # Borrowed and adapted from devtools: https://github.com/hadley/devtools/blob/ba7a5a4abd8258c52cb156e7b26bb4bf47a79f0b/R/utils.r#L74
