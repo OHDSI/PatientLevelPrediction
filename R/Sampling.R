c52fe30d45ab52bc55aa548546bdad3f426777d7
Solon Ioannou <sioann88@gmail.com>
2024-12-02 13:40:12 +0100

    Fixed spelling mistakes.

--- R/Sampling.R
+++ R/Sampling.R
@@ -23,10 +23,10 @@
 #'
 #' @param type              (character) Choice of:  \itemize{
 #'                                         \item 'none' No sampling is applied - this is the default
-#'                                         \item 'underSample' Undersample the non-outcome class to make the data more ballanced
+#'                                         \item 'underSample' Undersample the non-outcome class to make the data more balanced
 #'                                         \item 'overSample' Oversample the outcome class by adding in each outcome multiple times
 #'                                         } 
-#' @param numberOutcomestoNonOutcomes   (numeric) An numeric specifying the require number of outcomes per non-outcomes 
+#' @param numberOutcomestoNonOutcomes   (numeric) A numeric specifying the required number of outcomes per non-outcomes 
 #' @param sampleSeed         (numeric) A seed to use when splitting the data for reproducibility (if not set a random number will be generated)
 #'
 #' @return
