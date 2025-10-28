3c174fb3e7172617e224170b188296e6327fdf51
egillax <egillax@gmail.com>
2025-10-28 16:16:41 +0100

    instantiate python earlier

--- tests/testthat/test-sklearnClassifierSettings.R
+++ tests/testthat/test-sklearnClassifierSettings.R
@@ -17,6 +17,8 @@
 if (rlang::is_installed("reticulate") && identical(Sys.getenv("NOT_CRAN"), "true")) {
   if (Sys.getenv("GITHUB_ACTIONS") == "true") {
     reticulate::py_require("scikit-learn", python_version = Sys.getenv("RETICULATE_PY_VERSION"))
+    # force instantiation of python with correct version
+    sklearn <- reticulate::import("sklearn")
   } else {
     reticulate::py_require("scikit-learn")
   }
